04bb08bc4789735cb7a7daa4ba3f13dcb00dc15a
Abdsalam-Khalid <abdsalamkhalid785@gmail.com>
2025-11-10 22:01:36 +0100

    update

---
+++ Project/project.js
@@ -0,0 +1,16 @@
+let theme = document.querySelector('#theme-button');
+
+const changeTheme = () => {
+ 
+ if (document.body.style.background === 'white') {
+  document.body.style.background = '#42445A';
+  
+  document.body.style.color = 'white';
+ } else {
+  
+  document.body.style.background = 'white';
+  document.body.style.color = 'black';
+ }
+};
+
+theme.addEventListener('click', changeTheme);
